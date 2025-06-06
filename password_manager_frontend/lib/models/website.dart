9347b6a6476023e02f8b90f7606b1a3cd2d56056
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-06 12:29:28 +0300

    v_0_0_2: добавил: - изменил: website.dart удалил: -

--- password_manager_frontend/lib/models/website.dart
+++ password_manager_frontend/lib/models/website.dart
@@ -1,11 +1,8 @@
-
-
 import 'dart:typed_data';
 
 class Website {
   final int id;
-  final Uint8List passwordHash;
-  final Uint8List salt;
+  final String encryptedPassword;
   final String? websiteDescription;
   final String websiteLogin;
   final String websiteName;
@@ -18,8 +15,7 @@ class Website {
 
   Website({
     required this.id,
-    required this.passwordHash,
-    required this.salt,
+    required this.encryptedPassword,
     this.websiteDescription,
     required this.websiteLogin,
     required this.websiteName,
@@ -32,32 +28,34 @@ class Website {
   });
 
   factory Website.fromJson(Map<String, dynamic> json) => Website(
-    id: json['id'],
-    passwordHash: Uint8List.fromList(List<int>.from(json['password_hash'])),
-    salt: Uint8List.fromList(List<int>.from(json['salt'])),
-    websiteDescription: json['website_description'],
-    websiteLogin: json['website_login'],
-    websiteName: json['website_name'],
-    accountId: json['account_id'],
-    categoryId: json['category_id'],
-    websiteEmail: json['website_email'],
-    websiteUrl: json['website_url'],
-    createdAt: json['created_at'] != null ? DateTime.parse(json['created_at']) : null,
-    updatedAt: json['updated_at'] != null ? DateTime.parse(json['updated_at']) : null,
-  );
+        id: json['id'],
+        encryptedPassword: json['encrypted_password'],
+        websiteDescription: json['website_description'],
+        websiteLogin: json['website_login'],
+        websiteName: json['website_name'],
+        accountId: json['account_id'],
+        categoryId: json['category_id'],
+        websiteEmail: json['website_email'],
+        websiteUrl: json['website_url'],
+        createdAt: json['created_at'] != null
+            ? DateTime.parse(json['created_at'])
+            : null,
+        updatedAt: json['updated_at'] != null
+            ? DateTime.parse(json['updated_at'])
+            : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'password_hash': passwordHash,
-    'salt': salt,
-    'website_description': websiteDescription,
-    'website_login': websiteLogin,
-    'website_name': websiteName,
-    'account_id': accountId,
-    'category_id': categoryId,
-    'website_email': websiteEmail,
-    'website_url': websiteUrl,
-    'created_at': createdAt?.toIso8601String(),
-    'updated_at': updatedAt?.toIso8601String(),
-  };
+        'id': id,
+        'encrypted_password': encryptedPassword,
+        'website_description': websiteDescription,
+        'website_login': websiteLogin,
+        'website_name': websiteName,
+        'account_id': accountId,
+        'category_id': categoryId,
+        'website_email': websiteEmail,
+        'website_url': websiteUrl,
+        'created_at': createdAt?.toIso8601String(),
+        'updated_at': updatedAt?.toIso8601String(),
+      };
 }
