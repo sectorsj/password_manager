c619c9d71870882a190cd76e72084fd613f471aa
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-06 12:28:59 +0300

    v_0_0_2: добавил: - изменил: network_connection.dart удалил: -

--- password_manager_frontend/lib/models/network_connection.dart
+++ password_manager_frontend/lib/models/network_connection.dart
@@ -6,8 +6,7 @@ class NetworkConnection {
   final String? ipv4;
   final String? ipv6;
   final String networkConnectionLogin;
-  final Uint8List passwordHash;
-  final Uint8List salt;
+  final String encryptedPassword;
   final int accountId;
   final int categoryId;
   final String? networkConnectionDescription;
@@ -20,8 +19,7 @@ class NetworkConnection {
     this.ipv4,
     this.ipv6,
     required this.networkConnectionLogin,
-    required this.passwordHash,
-    required this.salt,
+    required this.encryptedPassword,
     required this.accountId,
     required this.categoryId,
     this.networkConnectionDescription,
@@ -29,33 +27,36 @@ class NetworkConnection {
     this.updatedAt,
   });
 
-  factory NetworkConnection.fromJson(Map<String, dynamic> json) => NetworkConnection(
-    id: json['id'],
-    networkConnectionName: json['network_connection_name'],
-    ipv4: json['ipv4'],
-    ipv6: json['ipv6'],
-    networkConnectionLogin: json['network_connection_login'],
-    passwordHash: Uint8List.fromList(List<int>.from(json['password_hash'])),
-    salt: Uint8List.fromList(List<int>.from(json['salt'])),
-    accountId: json['account_id'],
-    categoryId: json['category_id'],
-    networkConnectionDescription: json['network_connection_description'],
-    createdAt: json['created_at'] != null ? DateTime.parse(json['created_at']) : null,
-    updatedAt: json['updated_at'] != null ? DateTime.parse(json['updated_at']) : null,
-  );
+  factory NetworkConnection.fromJson(Map<String, dynamic> json) =>
+      NetworkConnection(
+        id: json['id'],
+        networkConnectionName: json['network_connection_name'],
+        ipv4: json['ipv4'],
+        ipv6: json['ipv6'],
+        networkConnectionLogin: json['network_connection_login'],
+        encryptedPassword: json['encrypted_password'],
+        accountId: json['account_id'],
+        categoryId: json['category_id'],
+        networkConnectionDescription: json['network_connection_description'],
+        createdAt: json['created_at'] != null
+            ? DateTime.parse(json['created_at'])
+            : null,
+        updatedAt: json['updated_at'] != null
+            ? DateTime.parse(json['updated_at'])
+            : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'network_connection_name': networkConnectionName,
-    'ipv4': ipv4,
-    'ipv6': ipv6,
-    'network_connection_login': networkConnectionLogin,
-    'password_hash': passwordHash,
-    'salt': salt,
-    'account_id': accountId,
-    'category_id': categoryId,
-    'network_connection_description': networkConnectionDescription,
-    'created_at': createdAt?.toIso8601String(),
-    'updated_at': updatedAt?.toIso8601String(),
-  };
+        'id': id,
+        'network_connection_name': networkConnectionName,
+        'ipv4': ipv4,
+        'ipv6': ipv6,
+        'network_connection_login': networkConnectionLogin,
+        'encryptedPassword': encryptedPassword,
+        'account_id': accountId,
+        'category_id': categoryId,
+        'network_connection_description': networkConnectionDescription,
+        'created_at': createdAt?.toIso8601String(),
+        'updated_at': updatedAt?.toIso8601String(),
+      };
 }
