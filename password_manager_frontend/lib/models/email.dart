d41dd01b6c82e90bbea27636d1476f5f20eb8f5f
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-06 12:25:56 +0300

    v_0_0_2: добавил: - изменил: email.dart удалил: -

--- password_manager_frontend/lib/models/email.dart
+++ password_manager_frontend/lib/models/email.dart
@@ -4,8 +4,7 @@ class Email {
   final int id;
   final String emailAddress;
   final String? emailDescription;
-  final Uint8List? passwordHash;  // Бинарный хэш пароля
-  final Uint8List? salt;          // Бинарная соль
+  final String encryptedPassword;
   final int accountId;
   final int? categoryId;
   final int? userId;
@@ -16,8 +15,7 @@ class Email {
     required this.id,
     required this.emailAddress,
     this.emailDescription,
-    this.passwordHash,
-    this.salt,
+    required this.encryptedPassword,
     required this.accountId,
     this.categoryId,
     this.userId,
@@ -26,28 +24,30 @@ class Email {
   });
 
   factory Email.fromJson(Map<String, dynamic> json) => Email(
-    id: json['id'],
-    emailAddress: json ['email_address'],
-    emailDescription: json ['email_description'],
-    passwordHash: json ['password_hash'] != null ? Uint8List.fromList(List<int>.from(json['password_hash'])) : null,
-    salt: json ['salt'] != null ? Uint8List.fromList(List<int>.from(json['salt'])) : null,
-    accountId: json ['account_id'],
-    categoryId: json ['category_id'],
-    userId: json ['user_id'],
-    createdAt: json['created_at'] != null ? DateTime.parse(json['created_at']) : null,
-    updatedAt: json['updated_at'] != null ? DateTime.parse(json['updated_at']) : null,
-  );
+        id: json['id'],
+        emailAddress: json['email_address'],
+        encryptedPassword: json['encrypted_password'] as String,
+        emailDescription: json['email_description'],
+        accountId: json['account_id'],
+        categoryId: json['category_id'],
+        userId: json['user_id'],
+        createdAt: json['created_at'] != null
+            ? DateTime.parse(json['created_at'])
+            : null,
+        updatedAt: json['updated_at'] != null
+            ? DateTime.parse(json['updated_at'])
+            : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'email_address': emailAddress,
-    'email_description': emailDescription,
-    'password_hash': passwordHash,
-    'salt': salt,
-    'account_id': accountId,
-    'category_id': categoryId,
-    'user_id': userId,
-    'created_at': createdAt?.toIso8601String(),
-    'updated_at': updatedAt?.toIso8601String(),
-  };
+        'id': id,
+        'email_address': emailAddress,
+        'email_description': emailDescription,
+        'encrypted_password': encryptedPassword,
+        'account_id': accountId,
+        'category_id': categoryId,
+        'user_id': userId,
+        'created_at': createdAt?.toIso8601String(),
+        'updated_at': updatedAt?.toIso8601String(),
+      };
 }
