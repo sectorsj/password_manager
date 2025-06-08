4500e92f377b7974379a00ccdc561d4dd6f20d38
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-08 10:31:22 +0300

    v_0_0_2: доделал logout на HomePage добавил: - изменил: home_page.dart удалил: -

--- password_manager_frontend/lib/pages/home_page.dart
+++ password_manager_frontend/lib/pages/home_page.dart
@@ -1,25 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:password_manager_frontend/models/account.dart';
 import 'package:password_manager_frontend/models/user.dart';
+import 'package:provider/provider.dart';
+import '../services/auth_service.dart';
 import '../tabs/emails_tab.dart';
 import '../tabs/network_connections_tab.dart';
 import '../tabs/websites_tab.dart';
+import '../utils/ui_routes.dart';
 
 class HomePage extends StatelessWidget {
   final Account account;
   final User user;
 
-  const HomePage({Key? key, required this.account, required this.user}) : super(key: key);
+  const HomePage({Key? key, required this.account, required this.user})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final String loginText = 'Привет, ${user.userName ?? 'пользователь'}';
+    final authService = Provider.of<AuthService>(context, listen: false);
 
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false, // ❌ отключаем стрелку назад
           title: Text(loginText),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              tooltip: 'Выйти',
+              onPressed: () async {
+                authService.clearSession();
+                if (context.mounted) {
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    UiRoutes.login,
+                    (route) => false,
+                  );
+                }
+              },
+            ),
+          ],
           bottom: const TabBar(
             tabs: [
               Tab(text: 'Emails'),
