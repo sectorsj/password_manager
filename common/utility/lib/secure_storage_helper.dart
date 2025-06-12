0ca296f714369202d3a177126070f41b276499b3
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-12 20:59:40 +0300

    v_0_0_2: Мелкие правки, писал тесты добавил: - изменил: hash_parser_test.dart, secure_storage_helper.dart удалил: -

--- common/utility/lib/secure_storage_helper.dart
+++ common/utility/lib/secure_storage_helper.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageHelper {
-  static const _secureStorage = FlutterSecureStorage();
+  static FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
   static const _aesKeyName = 'aes_key';
+
+  /// Позволяет подменить storage в тестах
+  static void overrideStorage(FlutterSecureStorage mockStorage) {
+    _secureStorage = mockStorage;
+  }
 
   static Future<String?> getAesKey() async {
     return await _secureStorage.read(key: _aesKeyName);
