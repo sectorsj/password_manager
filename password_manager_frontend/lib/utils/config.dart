4c584b265163bfafce885f238e22e1408b8b7710
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-06-16 10:57:42 +0300

    v_0_0_2 -> fix: релиз первого развертывания, а также воостановление файлов после неудачного merge добавил: - изменил: config.dart и др. файлы не .dart удалил: -

--- password_manager_frontend/lib/utils/config.dart
+++ password_manager_frontend/lib/utils/config.dart
@@ -1,1 +1,3 @@
-const String baseUrl = 'http://localhost:8080';
+const String baseUrlOld = 'http://localhost:8080';
+const String baseUrl = String.fromEnvironment('API_BASE_URL',
+    defaultValue: 'http://192.168.0.150:8080');
